aa3e8eae2fa0369bcb3f86d441518ff31d6582f6
Jerome Wynne <J.J.Wynne47@gmail.com>
2017-03-19 20:39:13 +0000

    Refined style, fixed major bugs; removed backwash method that was faulty (!)

--- assignment 1/Percolation.java
+++ assignment 1/Percolation.java
@@ -1,123 +1,74 @@
-//import edu.princeton.cs.algs4.StdRandom;
-//import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import edu.princeton.cs.algs4.StdDraw;
-import java.util.Random;
 
 public class Percolation {
-    public WeightedQuickUnionUF connectivity; // Associates grid variable with WQUUF object type
-    public boolean[][] nodes;
-    public int n_conn;
+    private WeightedQuickUnionUF connectivity;
+    private boolean[][] nodes;
+    private int nConn;
     
-    public Percolation(int n){
-       // Creates an nxn grid
+    public Percolation(int n) {
        if (n <= 0) throw new IllegalArgumentException();
-       n_conn = (int) Math.pow(n, 2) + 2;
-       connectivity = new WeightedQuickUnionUF(n_conn); // new creates the object, constructor initializes the object
+       nConn = (int) Math.pow(n, 2) + 2;
+       connectivity = new WeightedQuickUnionUF(nConn);
        nodes = new boolean[n][n];
-       for (int i = 0; i < nodes.length; i++){
-           for (int j = 0; i < nodes.length; i++) nodes[i][j] = false;
+       for (int i = 0; i < nodes.length; i++) {
+           for (int j = 0; j < nodes.length; j++) nodes[i][j] = false;
        }
     }
     
-    public void open(int row, int col){
-        // Opens site at row, col (1 indexed)
-        if (row*col == 0 || row > nodes.length || col > nodes.length)
+    private int ix2conn(int node_row, int node_col) {
+        return node_row*nodes.length + (node_col + 1);
+    }
+    
+    public void open(int row, int col) {
+        if (row <= 0 || col <= 0 || row > nodes.length || col > nodes.length) {
             throw new IndexOutOfBoundsException();
-        int p = (row - 1)*nodes.length + col; // posn. in connectvty.
+        }
         row -= 1;
         col -= 1;
+        int p = ix2conn(row, col);
         nodes[row][col] = true;
-        for (int i = Math.max(row - 1, 0); i <= Math.min(row + 1, nodes.length - 1); i++){
-            if (nodes[i][col]){
-               int q = nodes.length*i + col + 1;
-               connectivity.union(p, q);
-            }
-        }
-        for (int j = Math.max(col - 1, 0); j <= Math.min(col + 1, nodes.length - 1); j++){
-            if (nodes[row][j]){
-               int q = nodes.length*row + j + 1;
-               connectivity.union(p, q);
-            }
-        }
-        if (row == 0) connectivity.union(0, p);
-        if (row == nodes.length - 1 && isFull(row + 1, col +1)) connectivity.union(n_conn - 1, p);
+        int row_above = Math.max(row - 1, 0);
+        int row_below = Math.min(row + 1, nodes.length - 1);
+        int col_right = Math.min(col + 1, nodes.length - 1);
+        int col_left  = Math.max(col - 1, 0);
+        if (nodes[row_above][col])   connectivity.union(p, ix2conn(row_above, col));
+        if (nodes[row_below][col])   connectivity.union(p, ix2conn(row_below, col));
+        if (nodes[row][col_right])   connectivity.union(p, ix2conn(row, col_right));
+        if (nodes[row][col_left])    connectivity.union(p, ix2conn(row, col_left));
+        if (row == 0)                connectivity.union(0, p);
+        if (row + 1 == nodes.length) connectivity.union(nConn - 1, p);
     }
 
-    public boolean isOpen(int row, int col){
-        if (row*col == 0 || row > nodes.length || col > nodes.length)
+    public boolean isOpen(int row, int col) {
+        if (row <= 0 || col <= 0 || row > nodes.length || col > nodes.length) {
             throw new IndexOutOfBoundsException();
-        return nodes[row-1][col-1];
+        }
+        return nodes[row - 1][col - 1];
     }
 
-    public boolean isFull(int row, int col){
-        if (row*col == 0 || row > nodes.length || col > nodes.length)
+    public boolean isFull(int row, int col) {
+        if (row <= 0 || col <= 0 || row > nodes.length || col > nodes.length) {
             throw new IndexOutOfBoundsException();
-        int p = (row - 1)*nodes.length + col;
+        }
+        int p = ix2conn(row - 1, col - 1);
         return connectivity.connected(0, p);
-    }// is site (row, col) full? => Open site that can be connected to an open site in the top row via a chain of neighboring open sites
+    }
 
-    public int numberOfOpenSites(){
-        int n_open = 0;
-        for (int i = 0; i < nodes.length; i++){
-            for (int j = 0; j < nodes.length; j++){
-                if (nodes[i][j]) n_open += 1;
+    public int numberOfOpenSites() {
+        int nOpen = 0;
+        for (int i = 0; i < nodes.length; i++) {
+            for (int j = 0; j < nodes.length; j++) {
+                if (nodes[i][j]) nOpen += 1;
             }
         }
-        return n_open;
+        return nOpen;
     }
     
-    public boolean percolates(){
-        return connectivity.connected(0, n_conn - 1);
+    public boolean percolates() {
+        return connectivity.connected(0, nConn - 1);
     }
+     
+    public static void main(String[] args) {
+    } 
     
-    private void display(){
-        StdDraw.setScale(-.5, nodes.length - .5);
-        StdDraw.clear(StdDraw.BLACK);
-        StdDraw.setPenColor(StdDraw.WHITE);
-        // Draw nodes
-        for (int i = 0; i < nodes.length; i++){
-            for (int j = 0; j < nodes.length; j++){
-                if (isFull(i + 1, j + 1)){
-                    StdDraw.setPenColor(StdDraw.ORANGE);
-                    StdDraw.filledRectangle(j, nodes.length - i - 1, 0.49, 0.49);
-                }
-                else if (nodes[i][j]){
-                    StdDraw.setPenColor(StdDraw.WHITE);
-                    StdDraw.filledRectangle(j, nodes.length - i - 1, 0.49, 0.49);
-                }
-            }
-        }
-    }
-     
-    public static void main(String[] args) // optional test client
-    {
-     Percolation percolation = new Percolation(50);
-     
-     Random rng = new Random();
-     int n_open = 2200;
-     for (int i = 0; i < n_open; i++){
-         percolation.open(rng.nextInt(percolation.nodes.length)+1,
-                          rng.nextInt(percolation.nodes.length)+1);
-     }
-
-     // Tests
-     System.out.println("Site (1, 1) open? " + percolation.isOpen(1, 1));
-     System.out.println("Site (3, 4) open? " + percolation.isOpen(3, 4));
-     System.out.println("Site (1, 2) connected to top? " + percolation.isFull(1, 2));
-     System.out.println("Site (5, 3) connected to top? " + percolation.isFull(5, 3));
-     System.out.println("Site (4, 2) connected to top? " + percolation.isFull(4, 2));
-     System.out.println("Number of open sites: " + percolation.numberOfOpenSites());
-     System.out.println("Percolates: " + percolation.percolates());
-     percolation.display();
-     System.out.println("Run complete.");
-    }
-        
 }
-
-/* Corner cases: 
- * - Row and column indices are between 1 and n
- * - (1, 1) is upper-left site
- * - Throw java.lang.IndexOutOfBoundsException if any argument to isOpen() or isFull() is outside its range
- * - Throw java.lang.IllegalArgumentException if n <= 0
- */
--- assignment 1/PercolationStats.java
+++ assignment 1/PercolationStats.java
@@ -1,43 +1,44 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
-public class PercolationStats{
+public class PercolationStats {
     private double[] threshold;
     
-    public PercolationStats(int n, int trials){
+    public PercolationStats(int n, int trials) {
         if (n <= 0 || trials <= 0) throw new IllegalArgumentException();
         threshold = new double[trials];
-        for (int i = 0; i < trials; i++){
+        for (int i = 0; i < trials; i++) {
            Percolation perc = new Percolation(n);
-           while (!perc.percolates()){
-             int p = StdRandom.uniform(n)+1;
-             int q = StdRandom.uniform(n)+1;
+           while (!perc.percolates()) {
+             int p = StdRandom.uniform(n) + 1;
+             int q = StdRandom.uniform(n) + 1;
              perc.open(p, q);
            }
            threshold[i] = (double) perc.numberOfOpenSites()/(Math.pow(n, 2));
         }
     }
     
-    public double mean(){
+    public double mean() {
         return StdStats.mean(threshold);
     }
-    public double stddev(){
+    
+    public double stddev() {
         return StdStats.stddev(threshold);
     }
-    public double confidenceLo(){
+    
+    public double confidenceLo() {
         double mu = mean();
         double std = stddev();
         return mu - (1.96*std/(Math.sqrt(threshold.length)));
     }
-    public double confidenceHi(){
+    
+    public double confidenceHi() {
         double mu = mean();
         double std = stddev();
         return mu + (1.96*std/(Math.sqrt(threshold.length)));
     }
     
-    public static void main(String[] args){
-    // args[0] -> n for nxn grid
-    // args[1] -> T for T experiments
+    public static void main(String[] args) {
     int n = Integer.parseInt(args[0]);
     int T = Integer.parseInt(args[1]);
     PercolationStats test = new PercolationStats(n, T);
